4ecd0cc5fdac76eb3cc52baadfe95b2eac7bf957
btmiyashitasts <miyashitasts@pm.nttdata.co.jp>
2016-04-18 17:35:56 +0900

    change to 'NTT DATA Corporation'.#382
    (cherry picked from commit 0b8d2620f6391da631dc90438801d9ed213094f6)

--- terasoluna-tourreservation-selenium/src/test/java/org/terasoluna/tourreservation/tourreserve/common/constants/MessageKeysGen.java
+++ terasoluna-tourreservation-selenium/src/test/java/org/terasoluna/tourreservation/tourreserve/common/constants/MessageKeysGen.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013-2016 terasoluna.org
+ * Copyright (C) 2013-2016 NTT DATA Corporation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -48,7 +48,7 @@ public class MessageKeysGen {
 
         try {
             pw.println("/*");
-            pw.println(" * Copyright (C) 2013-2015 terasoluna.org");
+            pw.println(" * Copyright (C) 2013-2016 NTT DATA Corporation");
             pw.println(" *");
             pw.println(" * Licensed under the Apache License, Version 2.0 (the \"License\");"); 
             pw.println(" * you may not use this file except in compliance with the License."); 
