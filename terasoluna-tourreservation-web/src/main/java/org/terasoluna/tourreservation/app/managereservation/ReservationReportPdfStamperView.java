950acf667499d3eb8d8f287e46ab972de56bc089
btyanagawamg <yanagawamg@pm.nttdata.co.jp>
2019-02-28 10:29:34 +0900

    change to include dot in extension constant field #582

--- terasoluna-tourreservation-web/src/main/java/org/terasoluna/tourreservation/app/managereservation/ReservationReportPdfStamperView.java
+++ terasoluna-tourreservation-web/src/main/java/org/terasoluna/tourreservation/app/managereservation/ReservationReportPdfStamperView.java
@@ -32,7 +32,7 @@ public class ReservationReportPdfStamperView extends AbstractPdfStamperView {
 
     private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
 
-    private static final String downloadPDFExtension = "pdf";
+    private static final String downloadPDFExtension = ".pdf";
 
     @Value("${reservation.reportPdfUrl}")
     String reservationReportPdfUrl;
@@ -110,7 +110,7 @@ public class ReservationReportPdfStamperView extends AbstractPdfStamperView {
 
         response.setCharacterEncoding("UTF-8");
         response.setHeader("Content-Disposition", "attachment; filename="
-                + downloadPDFName + "." + downloadPDFExtension);
+                + downloadPDFName + downloadPDFExtension);
     }
 
 }
