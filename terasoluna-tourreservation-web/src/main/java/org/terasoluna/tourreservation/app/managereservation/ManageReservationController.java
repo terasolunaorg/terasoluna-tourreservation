b6419319abde47c57de0b0f97b077ff28e980659
Atsushi Yoshikawa <yoshikawaa@pm.nttdata.co.jp>
2019-03-18 16:36:34 +0900

    resolve deprecated api #614 (#615)

--- terasoluna-tourreservation-web/src/main/java/org/terasoluna/tourreservation/app/managereservation/ManageReservationController.java
+++ terasoluna-tourreservation-web/src/main/java/org/terasoluna/tourreservation/app/managereservation/ManageReservationController.java
@@ -38,7 +38,6 @@ import org.terasoluna.tourreservation.domain.service.reserve.ReserveService;
 import org.terasoluna.tourreservation.domain.service.userdetails.ReservationUserDetails;
 
 import javax.inject.Inject;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 
